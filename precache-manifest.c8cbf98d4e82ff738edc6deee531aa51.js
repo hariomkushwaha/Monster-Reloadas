2517e9df2d1d00513c919a15f6a5dc5bac2cea24
hariomkushwaha <hari2018cs048abesit@gmail.com>
2020-05-27 11:36:02 +0530

    Updates

---
+++ precache-manifest.c8cbf98d4e82ff738edc6deee531aa51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08b4bc18d7416d86518d0485e9311e7",
+    "url": "/Monster-Reloadas/index.html"
+  },
+  {
+    "revision": "76edf2c4e1b950865bef",
+    "url": "/Monster-Reloadas/static/css/main.5fe864ad.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/Monster-Reloadas/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Reloadas/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76edf2c4e1b950865bef",
+    "url": "/Monster-Reloadas/static/js/main.3914760f.chunk.js"
+  },
+  {
+    "revision": "c3c491c17060dc407c51",
+    "url": "/Monster-Reloadas/static/js/runtime-main.2f9750ce.js"
+  }
+]);
